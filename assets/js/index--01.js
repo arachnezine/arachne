af84e5dfd80535449b9d15014a62cc6bf7321369
afincato <andre.not.me@gmail.com>
2016-01-22 18:06:42 +0100

    fix opacity when hovering over elements of the index

--- assets/js/index--01.js
+++ assets/js/index--01.js
@@ -3,121 +3,121 @@ $(document).ready(function() {
     $("#preview--element__01").hover (function() {
       $("#preview--article__01").show();
       $(".title__bg h1").css("color", "#1111FF");
-      // $(".title__bg h1").css("visibility", "hidden");
+      $(".title__bg h1").css("opacity", "0.5");
     }, function() {
     	$("#preview--article__01").hide();
       $(".title__bg h1").css("color", "");
-      // $(".title__bg h1").css("visibility", "visible");
+      $(".title__bg h1").css("opacity", "1.0");
     });
 
     $("#preview--element__02").hover (function() {
       $("#preview--article__02").show();
       $(".title__bg h1").css("color", "#1111FF");
-      // $(".title__bg h1").css("visibility", "hidden");
+      $(".title__bg h1").css("opacity", "0.5");
     }, function() {
     	$("#preview--article__02").hide();
       $(".title__bg h1").css("color", "");
-      // $(".title__bg h1").css("visibility", "visible");
+      $(".title__bg h1").css("opacity", "1.0");
     });
 
     $("#preview--element__03").hover (function() {
       $("#preview--article__03").show();
       $(".title__bg h1").css("color", "#1111FF");
-      // $(".title__bg h1").css("visibility", "hidden");
+      $(".title__bg h1").css("opacity", "0.5");
     }, function() {
     	$("#preview--article__03").hide();
       $(".title__bg h1").css("color", "");
-      // $(".title__bg h1").css("visibility", "visible");
+      $(".title__bg h1").css("opacity", "1.0");
     });
 
     $("#preview--element__04").hover (function() {
       $("#preview--article__04").show();
       $(".title__bg h1").css("color", "#1111FF");
-      // $(".title__bg h1").css("visibility", "hidden");
+      $(".title__bg h1").css("opacity", "0.5");
     }, function() {
     	$("#preview--article__04").hide();
       $(".title__bg h1").css("color", "");
-      // $(".title__bg h1").css("visibility", "visible");
+      $(".title__bg h1").css("opacity", "1.0");
     });
 
     $("#preview--element__05").hover (function() {
       $("#preview--article__05").show();
       $(".title__bg h1").css("color", "#1111FF");
-      // $(".title__bg h1").css("visibility", "hidden");
+      $(".title__bg h1").css("opacity", "0.5");
     }, function() {
     	$("#preview--article__05").hide();
       $(".title__bg h1").css("color", "");
-      // $(".title__bg h1").css("visibility", "visible");
+      $(".title__bg h1").css("opacity", "1.0");
     });
 
     $("#preview--element__06").hover (function() {
       $("#preview--article__06").show();
       $(".title__bg h1").css("color", "#1111FF");
-      // $(".title__bg h1").css("visibility", "hidden");
+      $(".title__bg h1").css("opacity", "0.5");
     }, function() {
     	$("#preview--article__06").hide();
       $(".title__bg h1").css("color", "");
-      // $(".title__bg h1").css("visibility", "visible");
+      $(".title__bg h1").css("opacity", "1.0");
     });
 
     $("#preview--element__07").hover (function() {
       $("#preview--article__07").show();
       $(".title__bg h1").css("color", "#1111FF");
-      // $(".title__bg h1").css("visibility", "hidden");
+      $(".title__bg h1").css("opacity", "0.5");
     }, function() {
     	$("#preview--article__07").hide();
       $(".title__bg h1").css("color", "");
-      // $(".title__bg h1").css("visibility", "visible");
+      $(".title__bg h1").css("opacity", "1.0");
     });
 
     $("#preview--element__08").hover (function() {
       $("#preview--article__08").show();
       $(".title__bg h1").css("color", "#1111FF");
-      // $(".title__bg h1").css("visibility", "hidden");
+      $(".title__bg h1").css("opacity", "0.5");
     }, function() {
     	$("#preview--article__08").hide();
       $(".title__bg h1").css("color", "");
-      // $(".title__bg h1").css("visibility", "visible");
+      $(".title__bg h1").css("opacity", "1.0");
     });
 
     $("#preview--element__09").hover (function() {
       $("#preview--article__09").show();
       $(".title__bg h1").css("color", "#1111FF");
-      // $(".title__bg h1").css("visibility", "hidden");
+      $(".title__bg h1").css("opacity", "0.5");
     }, function() {
     	$("#preview--article__09").hide();
       $(".title__bg h1").css("color", "");
-      // $(".title__bg h1").css("visibility", "visible");
+      $(".title__bg h1").css("opacity", "1.0");
     });
 
     $("#preview--element__10").hover (function() {
       $("#preview--article__10").show();
       $(".title__bg h1").css("color", "#1111FF");
-      // $(".title__bg h1").css("visibility", "hidden");
+      $(".title__bg h1").css("opacity", "0.5");
     }, function() {
     	$("#preview--article__10").hide();
       $(".title__bg h1").css("color", "");
-      // $(".title__bg h1").css("visibility", "visible");
+      $(".title__bg h1").css("opacity", "1.0");
     });
 
     $("#preview--element__11").hover (function() {
       $("#preview--article__11").show();
       $(".title__bg h1").css("color", "#1111FF");
-      // $(".title__bg h1").css("visibility", "hidden");
+      $(".title__bg h1").css("opacity", "0.5");
     }, function() {
     	$("#preview--article__11").hide();
       $(".title__bg h1").css("color", "");
-      // $(".title__bg h1").css("visibility", "visible");
+      $(".title__bg h1").css("opacity", "1.0");
     });
 
     $("#preview--element__12").hover (function() {
       $("#preview--article__12").show();
       $(".title__bg h1").css("color", "#1111FF");
-      // $(".title__bg h1").css("visibility", "hidden");
+      $(".title__bg h1").css("opacity", "0.5");
     }, function() {
     	$("#preview--article__12").hide();
       $(".title__bg h1").css("color", "");
-      // $(".title__bg h1").css("visibility", "visible");
+      $(".title__bg h1").css("opacity", "1.0");
     });
 
 });
