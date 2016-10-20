39030f4111fe3ff2895e67100fdc54c5ecb705fd
afincato <andre.not.me@gmail.com>
2016-10-21 00:42:44 +0200

    not going anywhere with making wavesurfer.js responsive

--- issues/02/assets/js/scripts.js
+++ issues/02/assets/js/scripts.js
@@ -7,21 +7,21 @@ var wavesurfer = WaveSurfer.create({
 // wavesurfer.load('assets/six-poems_nadia-de-vries.mp3');
 wavesurfer.load('https://ia902606.us.archive.org/35/items/shortpoetry_047_librivox/song_cjrg_teasdale_64kb.mp3');
 
-var audio_player__wrapper = document.getElementsByClassName('audio-player--wrapper');
-var audio_player = document.getElementsByClassName('audio-player');
-
 wavesurfer.on('ready', function(){
     var w_h = $(window).height();
     var audio_player_pos_top = $('.nadia-de-vries__audio-player--container').offset().top;
     var waverform_width = w_h - audio_player_pos_top;
     console.log(w_h, audio_player_pos_top, waverform_width);
-    // $('#waveform').css('width', waverform_width);
-    // $('.nadia-de-vries__audio-player--wrapper').css('width', waverform_width);
     
     // temporary hack (works but visually not nice)
-    $('canvas').width(waverform_width * 1.1);
-    $('canvas').css('margin-top', 'auto');
-    $('canvas').css('margin-bottom', 'auto');
+    // $('canvas').width(waverform_width * 1.1);
+    // $('canvas').css('margin-top', 'auto');
+    // $('canvas').css('margin-bottom', 'auto');
+    
+    console.log(audio_player__wrapper, audio_player);
+
+    // $('canvas').attr('width', $(audio_player__wrapper).width() );
+    // $('canvas').attr('height', $(audio_player__wrapper).height() );
 });
 
 $('.nadia-de-vries__button__audio-player').on('click', function() {
