d3985b267e487ecc4328767626afbb34feb1f7a9
afincato <andre.not.me@gmail.com>
2016-10-27 16:42:56 +0200

    update styles and audio-player for Nadia poems

--- issues/02/assets/js/scripts.js
+++ issues/02/assets/js/scripts.js
@@ -1,27 +1,25 @@
 var wavesurfer = WaveSurfer.create({
     container: '#waveform',
-    waveColor: 'red',
-  	progressColor: 'purple'
+    waveColor: '#EA412C',
+  	progressColor: '#FFA131',
+    scrollParent: true,
+    hideScrollbar: true
 });
 
-// wavesurfer.load('assets/six-poems_nadia-de-vries.mp3');
-wavesurfer.load('https://ia902606.us.archive.org/35/items/shortpoetry_047_librivox/song_cjrg_teasdale_64kb.mp3');
+wavesurfer.load('assets/six-poems_nadia-de-vries.mp3');
+// wavesurfer.load('https://ia902606.us.archive.org/35/items/shortpoetry_047_librivox/song_cjrg_teasdale_64kb.mp3');
 
 wavesurfer.on('ready', function(){
     var w_h = $(window).height();
     var audio_player_pos_top = $('.nadia-de-vries__audio-player--container').offset().top;
     var waverform_width = w_h - audio_player_pos_top;
-    console.log(w_h, audio_player_pos_top, waverform_width);
     
     // temporary hack (works but visually not nice)
-    // $('canvas').width(waverform_width * 1.1);
-    // $('canvas').css('margin-top', 'auto');
-    // $('canvas').css('margin-bottom', 'auto');
-    
-    console.log(audio_player__wrapper, audio_player);
+    $('canvas').width(audio_player__wrapper);
+    $('canvas').css('margin-top', 'auto');
+    $('canvas').css('margin-bottom', 'auto');
 
-    // $('canvas').attr('width', $(audio_player__wrapper).width() );
-    // $('canvas').attr('height', $(audio_player__wrapper).height() );
+    console.log(audio_player__wrapper);
 });
 
 $('.nadia-de-vries__button__audio-player').on('click', function() {
