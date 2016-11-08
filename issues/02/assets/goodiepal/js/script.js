6a96045de8553e43998492483b9c366efe4b5091
afincato <andre.not.me@gmail.com>
2016-11-08 15:34:58 +0100

    delete /js for goodiepal

--- issues/02/assets/goodiepal/js/script.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-
-  $('.button__show-hide').on('click', function(){
-    $('.goodiepal__aside').toggleClass('goodiepal__aside--hide').toggleClass('goodiepal__aside--show');
-  })
-
-});
