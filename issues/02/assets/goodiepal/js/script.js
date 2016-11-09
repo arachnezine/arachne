ec3a410f4d3c0f849214c4f771190a565b014c56
afincato <andre.not.me@gmail.com>
2016-11-09 23:14:31 +0100

    finishing goodiepal sidebar thing

--- issues/02/assets/goodiepal/js/script.js
+++ issues/02/assets/goodiepal/js/script.js
@@ -2,24 +2,30 @@ $(document).ready(function() {
 
   var waypoint = new Waypoint({
     element: document.getElementById('essay__content'),
-    handler: function() {
-      $('.goodiepal__aside, .goodiepal__aside--button--collapse').toggleClass('goodiepal__aside--full-height');
-    },
-  })
+    handler: function(direction) {
+      $('.goodiepal__aside--expand').toggleClass('goodiepal__aside--full-height');
+    }
+  });
 
   $('.goodiepal__aside--button').on('click', function() {
     if($('.goodiepal__aside').hasClass('goodiepal__aside--expand')) {
       $('.goodiepal__aside').removeClass('goodiepal__aside--expand');
-      $('.goodiepal__aside--button').removeClass('goodiepal__aside--button__expand');
       $('.goodiepal__aside').addClass('goodiepal__aside--collapse');
+
+      $('.goodiepal__aside--button').removeClass('goodiepal__aside--button--expand');
       $('.goodiepal__aside--button').addClass('goodiepal__aside--button--collapse');
+
       $('.goodiepal__aside--button p').html('expand');
+
     } else if($('.goodiepal__aside').hasClass('goodiepal__aside--collapse')) {
       $('.goodiepal__aside').removeClass('goodiepal__aside--collapse');
-      $('.goodiepal__aside--button').removeClass('goodiepal__aside--button--collapse');
       $('.goodiepal__aside').addClass('goodiepal__aside--expand');
+      
+      $('.goodiepal__aside--button').removeClass('goodiepal__aside--button--collapse goodiepal__aside--full-height');
       $('.goodiepal__aside--button').addClass('goodiepal__aside--button--expand');
+
       $('.goodiepal__aside--button p').html('collapse');
     }
   });
+
 });
