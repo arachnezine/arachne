1d9709de9cf96c3e986231378e6a4a94ba3e9f88
afincato <andre.not.me@gmail.com>
2016-11-09 12:56:52 +0100

    set change button text when clicking on it

--- issues/02/assets/goodiepal/js/script.js
+++ issues/02/assets/goodiepal/js/script.js
@@ -6,11 +6,13 @@ $(document).ready(function() {
       $('.goodiepal__aside--button').removeClass('goodiepal__aside--button__expand');
       $('.goodiepal__aside').addClass('goodiepal__aside--collapse');
       $('.goodiepal__aside--button').addClass('goodiepal__aside--button--collapse');
+      $('.goodiepal__aside--button p').html('expand');
     } else if($('.goodiepal__aside').hasClass('goodiepal__aside--collapse')) {
       $('.goodiepal__aside').removeClass('goodiepal__aside--collapse');
       $('.goodiepal__aside--button').removeClass('goodiepal__aside--button--collapse');
       $('.goodiepal__aside').addClass('goodiepal__aside--expand');
       $('.goodiepal__aside--button').addClass('goodiepal__aside--button--expand');
+      $('.goodiepal__aside--button p').html('collapse');
     }
   });
 });
