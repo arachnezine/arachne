8fa6b469e62e2eda5ff0e17bdf7fa6e4bddcb65a
afincato <andre.not.me@gmail.com>
2016-11-14 00:15:42 +0100

    update script for Goodiepal (almost fixed 100%)

--- issues/03/assets/goodiepal/js/script.js
+++ issues/03/assets/goodiepal/js/script.js
@@ -3,7 +3,15 @@ $(document).ready(function() {
   var waypoint = new Waypoint({
     element: document.getElementById('essay__content'),
     handler: function(direction) {
-      $('.goodiepal__aside--expand').toggleClass('goodiepal__aside--full-height');
+      if (direction == 'down') {
+        $('.goodiepal__aside--expand').addClass('goodiepal__aside--full-height');
+      } else {
+        $('.goodiepal__aside--expand').removeClass('goodiepal__aside--full-height');
+        
+        $('.goodiepal__aside--button').on('click', function() {
+          $('.goodiepal__aside--expand').removeClass('goodiepal__aside--full-height');
+        });
+      }
     }
   });
 
@@ -16,7 +24,6 @@ $(document).ready(function() {
       $('.goodiepal__aside__wrapper').addClass('goodiepal__aside__wrapper--collapse');
 
       $('.goodiepal__aside--button p').html('expand');
-
     } else if($('.goodiepal__aside').hasClass('goodiepal__aside--collapse')) {
       $('.goodiepal__aside').removeClass('goodiepal__aside--collapse');
       $('.goodiepal__aside').addClass('goodiepal__aside--expand goodiepal__aside--full-height');
